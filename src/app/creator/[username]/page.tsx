18d041827a965cf6e010f970b77dbc352577c324
Marcos Vinicius <marcos.carini22@gmail.com>
2025-07-30 03:14:13 -0300

    feat: remove static user data from creator page

--- src/app/creator/[username]/page.tsx
+++ src/app/creator/[username]/page.tsx
@@ -21,7 +21,7 @@ export default async function Apoia({
     <div className=" min-h-[calc(100vh-64px)]">
       <div className="w-full h-64 relative bg-black">
         <Image
-          src={"https://github.com/devfraga.png"}
+          src={user.image ?? "https://github.com/marcos-carini.png"}
           alt="Banner"
           fill
           className="object-cover opacity-50"
@@ -33,7 +33,7 @@ export default async function Apoia({
       <section className="flex flex-col w-full items-center justify-center mx-auto max-w-7xl p-4 relative">
         <div className="flex flex-col items-center">
           <Image
-            src={"https://github.com/devfraga.png"}
+            src={user.image ?? "https://github.com/devfraga.png"}
             className="w-36 h-36 rounded-xl bg-gray-50 hover:shadow-lg duration-300 select-none text-zinc-900 text-3xl flex items-center justify-center object-cover absolute -top-16 border-4 border-white"
             alt="Matheus Fraga"
             width={96}
@@ -41,18 +41,18 @@ export default async function Apoia({
             quality={100}
           />
           <h1 className=" font-bold text-xl md:text-2xl mt-20 mb-4">
-            Fulano Dev
+            {user.name ?? "Sem nome"}
           </h1>
         </div>
       </section>
 
       <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-4 max-w-5xl">
-        <section className="hidden md:flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
+        <section className="flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
           <p className="font-semibold text-lg">
-            Sobre Fulano Dev
+            Sobre {user.name ?? ""}
           </p>
           <p className="text-gray-500 mt-2">
-            Descrição generica sobre o fulano dev
+            {user.bio ?? "Sem biografia"}
           </p>
         </section>
 
@@ -60,9 +60,8 @@ export default async function Apoia({
           className="bg-gray-50 rounded-md p-5 h-fit mx-2"
         >
           <h3 className="font-semibold text-lg">
-            Apoie o Matheus Fraga:
+            {user.name ? `Apoiar ${user.name}` : "Apoiar criador"}
           </h3>
-
 
         </section>
       </div>
