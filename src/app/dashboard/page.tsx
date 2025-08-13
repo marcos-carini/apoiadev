93a748662863464b963e802a7c5473ddb9a11471
Marcos Vinicius <marcos.carini22@gmail.com>
2025-08-13 17:52:55 -0300

    feat: add stripe dashboard express link for user in account management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { getLoginOnboardAccount } from "./_data-access/get-onboard-account";
 import { CreateAccountButton } from "./_components/create-account-button";
 import { getAllDonates } from "./_data-access/get-donates";
+import { getStripeDashboard } from "./_data-access/get-stripe-dashboard";
 
 
 
@@ -15,7 +16,8 @@ export default async function Dashboard() {
     redirect("/");
   }
 
-  const accountUrl = await getLoginOnboardAccount(session.user.connectedStripeAccountId);
+  // const accountUrl = await getLoginOnboardAccount(session.user.connectedStripeAccountId);
+  const urlStripeDashboard = await getStripeDashboard(session.user?.connectedStripeAccountId);
   const donates = await getAllDonates(session.user.id);
 
   return (
@@ -24,10 +26,12 @@ export default async function Dashboard() {
         <div className="w-full flex items-center gap-2 justify-between">
           <h1 className="text-2xl font-semibold">Minha conta</h1>
 
-          {accountUrl && (
+          {urlStripeDashboard && (
             <a
-              href={accountUrl}
-              className="bg-zinc-900 px-4 py-1 rounded-md text-white cursor-pointer">
+              href={urlStripeDashboard}
+              className="bg-zinc-900 px-4 py-1 rounded-md text-white cursor-pointer"
+              target="_blank"
+              >  
               Ajustar conta
           </a>
           )}
